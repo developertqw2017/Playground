cbffdfe740961fef3e7cb7008f0051e14a36e6ae
newthread-geek <geek@imbajin.com>
2017-11-10 23:45:31 +0800

    wangh

---
+++ data/non-payment.js
@@ -0,0 +1,30 @@
+var local_database = [{
+  imgSrc: "/images/food.jpg",
+  foodName: "武汉欢乐谷成人票(全日票)",
+  price: 190.00,
+  num: 1
+}, {
+  imgSrc: "/images/food.jpg",
+  foodName: "欢乐谷夜场成人票",
+  price: 200.00,
+  num: 3
+}, {
+  imgSrc: "/images/food.jpg",
+  foodName: "欢乐谷儿童票",
+  price: 140.00,
+  num: 8
+}, {
+  imgSrc: "/images/food.jpg",
+  foodName: "欢乐谷情侣年卡",
+  price: 766.00,
+  num: 8
+}, {
+  imgSrc: "/images/food.jpg",
+  foodName: "武汉欢乐谷单人行年卡",
+  price: 388.00,
+  num: 8
+}
+]
+module.exports = {
+  postList: local_database
+}
